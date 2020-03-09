1a1862801db396baa8f27d7dfc5ee7e8abde983b
senda488 <60650701+senda488@users.noreply.github.com>
2020-03-09 13:02:22 +0900

    S3削除

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '18.176.180.143', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,6 +60,6 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.176.180.143', user: 'ec2-user', roles: %w{app db web}
+
 
 
