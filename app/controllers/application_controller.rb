70842f43967c7ab4ee0c1ff4d3981f0d59f79bed
senda488 <60650701+senda488@users.noreply.github.com>
2020-03-10 14:02:03 +0900

    basic 本番環境のみに

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth #, if: :production?
   protect_from_forgery with: :exception
   
-  # private
+  private
 
-  # def production?
-  #   Rails.env.production?
-  # end
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
