5d425441a32d5f4012c46e06f6867ea7c3ac93f5
masuda0423 <blewnac@gmail.com>
2020-03-12 12:00:49 +0900

    新規登録ページ、ログインページ　実装.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth,  if: :production?
+  protect_from_forgery with: :exception
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday_year, :birthday_month, :birthday_day])
-  before_action :basic_auth,  if: :production?
-  protect_from_forgery with: :exception
-  
-  private
+  end
 
+  private
   def production?
     Rails.env.production?
   end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
