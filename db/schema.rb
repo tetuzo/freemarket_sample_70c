2f9d0fc0147a7fb0580416d1305801204915839f
senda488 <60650701+senda488@users.noreply.github.com>
2020-03-18 12:41:40 +0900

    deploy前の変更反映
    スプリントレビューのため

--- db/schema.rb
+++ db/schema.rb
@@ -104,13 +104,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_031501) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "trades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "seller_id"
-    t.integer "buyer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
