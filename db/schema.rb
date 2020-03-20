35b64092fa40d9e4bec18ba0f0a9dd5731f02826
afhnjkl <hmktrs3@gmail.com>
2020-03-20 17:20:00 +0900

    商品購入ー修正

--- db/schema.rb
+++ db/schema.rb
@@ -66,17 +66,13 @@ ActiveRecord::Schema.define(version: 2020_03_19_044522) do
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "seller_id"
-    t.bigint "buyer_id"
+    t.integer "category_id"
     t.integer "size_id"
     t.integer "shipping_charges_id"
     t.integer "shipping_days_id"
     t.integer "status_id"
-    t.integer "category_id"
     t.string "brand_id"
     t.integer "prefecture_id"
-    t.index ["buyer_id"], name: "index_items_on_buyer_id"
-    t.index ["seller_id"], name: "index_items_on_seller_id"
   end
 
   create_table "personals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -100,13 +96,11 @@ ActiveRecord::Schema.define(version: 2020_03_19_044522) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
+  create_table "trades", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "seller_id"
+    t.integer "buyer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -124,5 +118,4 @@ ActiveRecord::Schema.define(version: 2020_03_19_044522) do
 
   add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
-  add_foreign_key "sns_credentials", "users"
 end
