ce32370a22c98b31642c922a6e38de4f543fef88
senda488 <60650701+senda488@users.noreply.github.com>
2020-03-23 17:06:16 +0900

    items seller_idダブリ解消
    古いマイグレーション ファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_023205) do
+ActiveRecord::Schema.define(version: 2020_03_20_121430) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "destination_family_name", null: false
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2020_03_23_023205) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -114,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_023205) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
